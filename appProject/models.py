0ac82796e6a53524566f6e527b2cac9d37210309
Jon Zatón <j.zaton@opendeusto.es>
2020-04-07 14:03:39 +0200

    clase componentes

--- appProject/models.py
+++ appProject/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+#PEDIDO
+
+
+
+#PRODUCTOS
+
+
+
+
+#COMPONENTES
+class Componentes (models.Model):
+
+    codigo_referencia = models.IntegerField()
+    nombre_modelo = models.CharField(max_length=50)
+    marca = models.CharField(max_length=50)
