427858e7f1eba0bf04258801ee6d7dc1a59b2937
Unknown <eneko.galdos@opendeusto.es>
2020-04-10 22:58:20 +0200

    Visualizar lista pedidos y detalle

--- appProject/urls.py
+++ appProject/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('productos/', views.ProductosListView.as_view(), name='index'),
     path('producto/<int:pk>/', views.ProductoDetailView.as_view(), name='producto'),
     path('producto/create/', views.CreateProductoView.as_view(), name='producto_create'),
+    path('pedidos/', views.PedidosListView.as_view(), name='pedidos'),
+    path('pedido/<int:pk>/', views.PedidoDetailView.as_view(), name='pedido'),
+    path('pedido/create/', views.CreatePedidosView.as_view(), name='pedido_create'),
  ]
--- appProject/views.py
+++ appProject/views.py
@@ -1,10 +1,15 @@
-from appProject.forms import ProductoForm
+from appProject.forms import ProductoForm, PedidoForm
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 
 from appProject.models import Producto
+from appProject.models import Pedido
 from django.views.generic.base import View
+
+
+
+
 
 
 class ProductosListView(ListView):
@@ -44,3 +49,47 @@ class CreateProductoView(View):
             return redirect('index')
 
         return render(request, 'producto_create.html', {'form': form})
+
+
+class PedidosListView(ListView):
+    model = Pedido
+    template_name = 'pedidos.html'
+    queryset = Pedido.objects.order_by('codigo_referencia')
+    context_object_name = 'lista_pedidos'
+
+    def get_context_data(self, **kwargs):
+        context = super(PedidosListView, self).get_context_data(**kwargs)
+        context['titulo_pagina'] = 'Pedidos'
+        return context
+
+
+class PedidoDetailView(DetailView):
+    model = Pedido
+    template_name = 'pedido.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(PedidoDetailView, self).get_context_data(**kwargs)
+        context['titulo_pagina'] = 'Detalles del Pedido'
+        return context
+
+
+class CreatePedidosView(View):
+    def get(self, request, *args, **kwargs):
+        form = PedidoForm()
+        context = {
+            'form': form,
+            'titulo_pagina': 'Añadir nuevo pedido'
+        }
+        return render(request, 'pedido_create.html', context)
+
+    def post(self, request, *args, **kwargs):
+        form = PedidoForm(request.POST)
+
+        if form.is_valid():
+            print("hola")
+            form.save()
+            return redirect('pedidos')
+
+        return render(request, 'pedido_create.html', {'form': form})
+
+
